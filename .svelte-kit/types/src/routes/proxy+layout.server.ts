69072cc3daeafee7b44da2001f17be770d7eab37
AntiPhil <philipp-stehle@outlook.de>
2026-04-01 09:14:50 +0200

    Remove node_modules/.sveltekit from repository

--- .svelte-kit/types/src/routes/proxy+layout.server.ts
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-import type { LayoutServerLoad } from './$types';
-
-// Stellt die Session auf allen Seiten als page.data.session bereit
-export const load = async (event: Parameters<LayoutServerLoad>[0]) => {
-	return {
-		session: await event.locals.auth(),
-	};
-};
